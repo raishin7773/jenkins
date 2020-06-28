efe2fa44aba6ed0d00a665b0019371da0996383a
raishin <raishin7773@yahoo.co.jp>
2020-06-28 10:32:16 +0900

    first commit

--- src/main/java/com/raishin/jenkins/HelloController.java
+++ src/main/java/com/raishin/jenkins/HelloController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HelloController {
     @GetMapping("/hello")
     public String getHello() {
+        System.out.println("change!");
         return "hello";
     }
 }
